c71679b7eb2f9cc0334177ec9b2eceb23ea15d55
steven-yao24 <153370050+steven-yao24@users.noreply.github.com>
2024-01-14 20:53:48 -0800

    Add files via upload

--- momentum_pca.m
+++ momentum_pca.m
@@ -12,7 +12,7 @@ k = 3;
 [G,jdate]=findgroups(return_monthly.date);
 return_monthly.jdate = G;
 T = size(jdate,1);
-mom = table();
+momcoef = table();
 
 for i = k:T-1
     % get previous return data
@@ -33,34 +33,34 @@ for i = k:T-1
     data_now = return_monthly(index_now,:);
     mom_temp = outerjoin(data_now, mom_now,'Keys',{'code'},'MergeKeys',true,'Type','left');
     
-    mom = vertcat(mom,mom_temp);
+    momcoef = vertcat(momcoef,mom_temp);
 end
 
 
 % construct 5 portfolios
-[G,jdate]=findgroups(mom.jdate);
+[G,jdate]=findgroups(momcoef.jdate);
 mom_breaks = table(jdate);
 N=5; 
-momport = zeros(height(mom),1);
+momport = zeros(height(momcoef),1);
 for i=1:N
    underprctile = @(input) prctile(input,100/N*(i-1)); 
    upperprctile = @(input) prctile(input,100/N*i);
    
-   mom_breaks.volunder = splitapply(underprctile,mom.mom_now,G);
-   mom_breaks.volupper = splitapply(upperprctile,mom.mom_now,G);
+   mom_breaks.momunder = splitapply(underprctile,momcoef.mom_now,G);
+   mom_breaks.momupper = splitapply(upperprctile,momcoef.mom_now,G);
    
-   merge_data = outerjoin(mom,mom_breaks,'Keys',{'jdate'},'MergeKeys',true,'Type','left');
+   merge_data = outerjoin(momcoef,mom_breaks,'Keys',{'jdate'},'MergeKeys',true,'Type','left');
    
-   mom_bucket_i = @(mom,volunder,volupper) bucket(mom,volunder,volupper,i);
+   mom_bucket_i = @(mom,momunder,momupper) bucket(mom,momunder,momupper,i);
    %assigh portfolio
-   momport_i = rowfun(mom_bucket_i, merge_data(:,{'mom_now','volunder','volupper'}),...
+   momport_i = rowfun(mom_bucket_i, merge_data(:,{'mom_now','momunder','momupper'}),...
        'OutputFormat','cell');
    
    momport_i = cell2mat(momport_i);
    momport = momport+momport_i;
    
-   merge_data.volunder=[];
-   merge_data.volupper=[];
+   merge_data.momunder=[];
+   merge_data.momupper=[];
 end
 merge_data.momport = momport;
 
@@ -79,10 +79,19 @@ factors = returns * coefMatrix(:,1:3);
 pca1 = factors(:,1);
 pca2 = factors(:,2);
 
-
-plot(1:5, coefMatrix(:,1:3), '-x');
+plot(1:5, coefMatrix(:,1:2), '-x');
 line([0 125], [0 0], 'Color', 'k'); 
 axis([1 5 -0.8 0.8]); 
+legend('off');
+legend('pca1','pca2');
 ylabel('Factor Loading'); 
 print('pca3', '-depsc');
+
+% calculate Var
+M = cov(returns);
+momcoef = [-sqrt(2)/2 0 0 0 sqrt(2)/2];
+varpca = coefMatrix(:,1:2)'*M*coefMatrix(:,1:2);
+varpca1 = varpca(1,1);
+varpca2 = varpca(2,2);
+varmom = momcoef*M*momcoef';
 
